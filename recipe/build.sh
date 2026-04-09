80663f8368073287b5fe74c1ded84edfcfe9dbdd
Ryan Clary <mrclary@users.noreply.github.com>
2026-04-09 07:56:29 -0700

    next attempt

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ex
 
-export CC=/opt/homebrew/bin/gcc-15
-export CXX=/opt/homebrew/bin/g++-15
-export FC=/opt/homebrew/bin/gfortran
-export F77=$FC
+# export CC=/opt/homebrew/bin/gcc-15
+# export CXX=/opt/homebrew/bin/g++-15
+# export FC=/opt/homebrew/bin/gfortran
+# export F77=$FC
 
-export LDFLAGS="${LDFLAGS} -Wl,-headerpad_max_install_names"
-export FCFLAGS="${FCFLAGS} -fPIC"
+# export LDFLAGS="${LDFLAGS} -Wl,-headerpad_max_install_names"
+# export FCFLAGS="${FCFLAGS} -fPIC"
 
-$PYTHON -m pip install . --no-build-isolation --no-deps -vv
+# $PYTHON -m pip install . --no-build-isolation --no-deps -vv
+$PYTHON -m pip install --no-deps -vv dist/bacoli*.whl
